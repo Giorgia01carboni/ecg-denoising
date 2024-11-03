78568ac1326775335a495c0401269024e67f8ca7
giorgia01carboni <giorgia.carboni@gmail.com>
2024-11-03 23:23:26 +0100

    final code

--- ecg_denoising.py
+++ ecg_denoising.py
@@ -16,14 +16,18 @@ delta_t = 1 / fs
 time_vector = np.arange(0, delta_t*N, delta_t)
 
 plt.plot(time_vector, ecg_values)
+plt.title("Original ECG Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude (mV)")
 plt.show()
 
 # Window selected: Hamming
 # Window length L = (4 / 20 Hz) * 500 Hz (should be approximated to first odd number 101)
+# Window Length is based on transition band width
 
 L = int(np.ceil((4 / 20) * fs))
 if L % 2 == 0:
-    L += 1
+    L += 1      # Makes sure L value is odd
 
 # window vector
 w = np.hamming(L)
@@ -37,9 +41,11 @@ w_zero_phase = np.roll(w_padded, -max_index + N // 2)
 
 plt.plot(w_zero_phase)
 plt.title("Hamming Window (Zero-Phase)")
+plt.xlabel("Sample Index")
+plt.ylabel("Amplitude")
 plt.show()
 
-# cutoff frequencies:
+# cutoff frequencies for the bandpass filter:
 # Lower cutoff frequency: 0.5Hz
 # Upper cutoff frequency: 40Hz
 
@@ -47,21 +53,22 @@ plt.show()
 upper_cutoff = 40 / (fs/2)
 lower_cutoff = 0.5 / (fs/2)
 
-# Filter vector h
-time_vector_h = np.arange(-L/2, L/2)
-lower_sinc = np.sinc(2 * time_vector_h * lower_cutoff)
-upper_sinc = np.sinc(2 * time_vector_h * upper_cutoff)
+# Time vector for filter h
+time_vector_h = np.arange(-L//2, L//2)
+lower_sinc = np.sinc(2 * time_vector_h * upper_cutoff)          # allow frequencies below 40 Hz to pass
 
-# delta function
+# delta function and design of high-pass filter
 delta = np.zeros(N)
 delta[0] = 1
 
-h_high = delta - lower_sinc
+h_low = np.sinc(2 * time_vector_h * lower_cutoff)      # low-pass filter with a cutoff at 0.5 Hz
+h_low_padded = np.pad(h_low, (0, N - len(h_low)), 'constant')
+h_high = delta - h_low_padded          # blocks frequencies below 0.5 Hz and allows frequencies above 0.5 Hz to pass
 
-#h = upper_sinc - lower_sinc
+# bandpass filter * zero-phase window = impulse response
+h_bandpass = np.fft.ifft(np.fft.fft(h_low_padded) * np.fft.fft(h_high)).real
+h_filtered = h_bandpass * w_zero_phase
 
-#h_filtered = w * h_high
-h_bandpass = np.fft.ifft(np.fft.fft(lower_sinc) * np.fft.fft(h_high)).real
 H = np.fft.fft(h_bandpass)
 H_filtered = np.fft.fft(h_bandpass * w_zero_phase)
 
@@ -77,5 +84,5 @@ plt.show()
 
 # Filter ecg using circular convolution
 ecg_filtered = np.convolve(ecg_values, H_filtered, mode='same')
-plt.plot(ecg_filtered)
+plt.plot(time_vector, ecg_filtered)
 plt.show()
